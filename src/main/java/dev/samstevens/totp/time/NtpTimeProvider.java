408d5230a567a8f09460f26c768d629ce937c816
Sam Stevens <samdjstevens@googlemail.com>
2019-12-09 19:07:57 +0000

    move apache net commons to be an optional dependency

--- src/main/java/dev/samstevens/totp/time/NtpTimeProvider.java
+++ src/main/java/dev/samstevens/totp/time/NtpTimeProvider.java
@@ -17,6 +17,19 @@ public class NtpTimeProvider implements TimeProvider {
     }
 
     public NtpTimeProvider(String ntpHostname, int timeout) throws UnknownHostException {
+        this(ntpHostname, timeout, "org.apache.commons.net.ntp.NTPUDPClient");
+    }
+
+    // Package-private, for tests only
+    NtpTimeProvider(String ntpHostname, String dependentClass) throws UnknownHostException {
+        // default timeout of  3 seconds
+        this(ntpHostname, 3000, dependentClass);
+    }
+
+    private NtpTimeProvider(String ntpHostname, int timeout, String dependentClass) throws UnknownHostException {
+        // Check the optional commons-net dependency is on the classpath
+        checkHasDependency(dependentClass);
+
         client = new NTPUDPClient();
         client.setDefaultTimeout(timeout);
         ntpHost = InetAddress.getByName(ntpHostname);
@@ -30,6 +43,14 @@ public class NtpTimeProvider implements TimeProvider {
             return (long) Math.floor(timeInfo.getReturnTime() / 1000L);
         } catch (Exception e) {
             throw new TimeProviderException("Failed to provide time from NTP server. See nested exception.", e);
+        }
+    }
+
+    private void checkHasDependency(String dependentClass) {
+        try {
+            Class<?> ntpClientClass = Class.forName(dependentClass);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("The Apache Commons Net library must be on the classpath to use the NtpTimeProvider.");
         }
     }
 }
